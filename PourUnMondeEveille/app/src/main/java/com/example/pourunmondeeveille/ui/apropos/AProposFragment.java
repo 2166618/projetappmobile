82640d69c2e17bf99674fdbdb591f2d0e4bef3d2
Veronique Tavares <125508905+2166618@users.noreply.github.com>
2024-05-23 16:41:39 -0400

    PUME-11 Ajustement du menu

---
+++ PourUnMondeEveille/app/src/main/java/com/example/pourunmondeeveille/ui/apropos/AProposFragment.java
@@ -0,0 +1,37 @@
+package com.example.pourunmondeeveille.ui.apropos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.pourunmondeeveille.databinding.FragmentAProposBinding;
+
+public class AProposFragment extends Fragment {
+
+    private FragmentAProposBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AProposViewModel AProposViewModel =
+                new ViewModelProvider(this).get(AProposViewModel.class);
+
+        binding = FragmentAProposBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textAPropos;
+        AProposViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- PourUnMondeEveille/app/src/main/java/com/example/pourunmondeeveille/ui/apropos/AProposViewModel.java
+++ PourUnMondeEveille/app/src/main/java/com/example/pourunmondeeveille/ui/apropos/AProposViewModel.java
@@ -1,16 +1,16 @@
-package com.example.pourunmondeeveille.ui.tableaudebord;
+package com.example.pourunmondeeveille.ui.apropos;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class TableauDeBordViewModel extends ViewModel {
+public class AProposViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public TableauDeBordViewModel() {
+    public AProposViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is Tableau de bord fragment");
+        mText.setValue("This is a propos fragment");
     }
 
     public LiveData<String> getText() {
